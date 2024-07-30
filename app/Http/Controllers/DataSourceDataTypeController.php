c7cfbcb6a09818f2be10db4ef613caf8b17f0e3a
mrdbidwill@gmail.com <mrdbidwill@gmail.com>
2024-07-30 06:49:37 -0500

    Added all controllers to web.php

--- app/Http/Controllers/DataSourceDataTypeController.php
+++ app/Http/Controllers/DataSourceDataTypeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DataSourceDataTypesController extends Controller
+class DataSourceDataTypeController extends Controller
 {
     public function index() {}
 
--- app/Http/Controllers/ImageSpecimenThumbnailController.php
+++ app/Http/Controllers/ImageSpecimenThumbnailController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ImageSpecimeThumbnailController extends Controller
+class ImageSpecimenThumbnailController extends Controller
 {
     public function index() {}
 
