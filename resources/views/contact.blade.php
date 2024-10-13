d49282ae6de93080141800b112382a28d378c967
Will Johnston <129679679+mrdbidwill@users.noreply.github.com>
2024-10-13 16:20:16 -0500

    format contact email form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,30 +6,43 @@
 
     <form action="{{ route('contact.submit') }}" method="POST">
         @csrf
-        <label for="name">Name:</label> <input type="text" id="name" name="name" value="{{ old('name') }}">
-        @error('name')
-        <div>{{ $message }}</div>
-        @enderror
+        <div class="border-2 border-amber-400 bg-amber-200 m-auto p-4">
+            <label for="name">Name:</label> <input type="text" id="name" name="name" size="80"
+                                                   value="{{ old('name') }}">
+            @error('name')
+            <div>{{ $message }}</div>
+            @enderror
+        </div>
 
-        <label for="email">Email:</label> <input type="email" id="email" name="email" value="{{ old('email') }}">
-        @error('email')
-        <div>{{ $message }}</div>
-        @enderror
+        <div class="border-2 border-amber-400 bg-amber-200 m-auto p-4">
+            <label for="email">Email:</label> <input type="email" id="email" name="email" size="80"
+                                                     value="{{ old('email') }}">
+            @error('email')
+            <div>{{ $message }}</div>
+            @enderror
+        </div>
 
-        <label for="recipient">Send to:</label> <select id="recipient" name="recipient">
-            <option value="contact">Contact</option>
-            <option value="webmaster">Webmaster</option>
-            <option value="will">Will</option>
-        </select>
-        @error('recipient')
-        <div>{{ $message }}</div>
-        @enderror
+        <div class="border-2 border-amber-400 bg-amber-200 m-auto p-4">
+            <label for="recipient">Send to:</label> <select id="recipient" name="recipient">
+                <option value="contact">Contact</option>
+                <option value="webmaster">Webmaster</option>
+                <option value="will">Will</option>
+            </select>
+            @error('recipient')
+            <div>{{ $message }}</div>
+            @enderror
+        </div>
 
-        <label for="message">Message:</label> <textarea id="message" name="message">{{ old('message') }}</textarea>
-        @error('message')
-        <div>{{ $message }}</div>
-        @enderror
+        <div class="border-2 border-amber-400 bg-amber-200 m-auto p-4">
+            <label for="message">Message:</label> <textarea id="message" name="message" rows="12"
+                                                            cols="80">{{ old('message') }}</textarea>
+            @error('message')
+            <div>{{ $message }}</div>
+            @enderror
+        </div>
 
-        <button type="submit">Send</button>
+        <div class="border-2 border-amber-400 bg-amber-200 m-auto p-2 text-center font-extrabold">
+            <button type="submit">Send</button>
+        </div>
     </form>
 </x-layout>
