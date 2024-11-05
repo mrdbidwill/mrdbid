7c7fe7ee6a08150fed5205b33078c499808afc22
Will Johnston <129679679+mrdbidwill@users.noreply.github.com>
2024-11-04 18:48:10 -0600

    modified images to work - also mobile links

--- resources/views/components/site-nav-bar.blade.php
+++ resources/views/components/site-nav-bar.blade.php
@@ -1,59 +1,106 @@
 @props(['active' => false])
-<nav class="bg-gray-100">
-    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="flex h-16 items-center justify-between">
-            <div class="flex items-center">
-                <div class="flex-shrink-0">
-                    <a href="{{ url('/') }}">
-                        <x-application-logo/>
-                    </a>
-                </div>
+<nav class="bg-gray-100 p-4">
+    <div class="container mx-auto flex justify-between items-center">
 
-                <div class="hidden md:block">
-                    <div class="ml-10 flex items-baseline space-x-4">
-                        <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-                        <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
-                        <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
-                        <x-nav-link href="/books" :active="request()->is('books')">Books</x-nav-link>
-
-                        @auth
-                            <x-nav-link href="{{ route('specimens.index') }}"
-                                        :active="request()->is('specimens.index')">
-                                Specimens
-                            </x-nav-link>
-
-                            @php
-                                $member_type = \App\Utils\MemberUtils::get_member_type();
-                            @endphp
-                            @if($member_type < 4)
-                                <x-nav-link href="{{ route('admin_dashboard') }}" style="color: red; font-weight: bold;"
-                                            :active="request()->is('admin_dashboard')">
-                                    Admin
-                                </x-nav-link>
-                            @endif
-
-                            <x-nav-link href="{{ route('profile.edit') }}"
-                                        :active="request()->is('profile.edit')"> {{  Auth::user()->name }}
-                            </x-nav-link>
-
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <x-nav-link href="route('logout')"
-                                            onclick="event.preventDefault();  this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                                </x-nav-link>
-                            </form>
-
-                        @endauth
-
-                        @guest
-                            <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
-                            <x-nav-link href="/register" :active="request()->is('register')">Register
-                            </x-nav-link>
-                        @endguest
-                    </div>
-                </div>
-            </div>
+        <div class="flex-shrink-0">
+            <a href="{{ url('/') }}">
+                <x-application-logo/>
+            </a>
         </div>
+
+        <!-- Standard navigation links -->
+        <div class="hidden md:flex space-x-4">
+
+            <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+            <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+            <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+            <x-nav-link href="/books" :active="request()->is('books')">Books</x-nav-link>
+
+            @auth
+                <x-nav-link href="{{ route('specimens.index') }}" :active="request()->is('specimens.index')">
+                    Specimens
+                </x-nav-link>
+                @php
+                    $member_type = \App\Utils\MemberUtils::get_member_type();
+                @endphp
+                @if($member_type < 4)
+                    <x-nav-link href="{{ route('admin_dashboard') }}" style="color: red; font-weight: bold;"
+                                :active="request()->is('admin_dashboard')">
+                        Admin
+                    </x-nav-link>
+                @endif
+
+                <x-nav-link href="{{ route('profile.edit') }}"
+                            :active="request()->is('profile.edit')"> {{  Auth::user()->name }}
+                </x-nav-link>
+
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <x-nav-link href="route('logout')"
+                                onclick="event.preventDefault();  this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-nav-link>
+                </form>
+
+            @endauth
+
+            @guest
+                <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
+                <x-nav-link href="/register" :active="request()->is('register')">Register
+                </x-nav-link>
+            @endguest
+        </div>
+        <!-- End Standard navigation links -->
+
+        <!-- Mobile navigation links -->
+        <div id="site-menu" class="hidden md:hidden">
+
+            <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+            <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+            <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+            <x-nav-link href="/books" :active="request()->is('books')">Books</x-nav-link>
+
+            @auth
+                <x-nav-link href="{{ route('specimens.index') }}" :active="request()->is('specimens.index')">
+                    Specimens
+                </x-nav-link>
+                @php
+                    $member_type = \App\Utils\MemberUtils::get_member_type();
+                @endphp
+                @if($member_type < 4)
+                    <x-nav-link href="{{ route('admin_dashboard') }}" style="color: red; font-weight: bold;"
+                                :active="request()->is('admin_dashboard')">
+                        Admin
+                    </x-nav-link>
+                @endif
+
+                <x-nav-link href="{{ route('profile.edit') }}"
+                            :active="request()->is('profile.edit')"> {{  Auth::user()->name }}
+                </x-nav-link>
+
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <x-nav-link href="route('logout')"
+                                onclick="event.preventDefault();  this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-nav-link>
+                </form>
+
+            @endauth
+
+            @guest
+                <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
+                <x-nav-link href="/register" :active="request()->is('register')">Register
+                </x-nav-link>
+            @endguest
+        </div>
+        <!-- End Mobile navigation links -->
     </div>
 </nav>
+
+<script>
+    function toggleMenu(menuId) {
+        var element = document.getElementById(menuId);
+        element.classList.toggle("hidden");
+    }
+</script>
--- resources/views/components/specimens-nav-bar.blade.php
+++ resources/views/components/specimens-nav-bar.blade.php
@@ -2,35 +2,96 @@
     $isSpecimens = request()->routeIs('specimens.index');
 @endphp
 
-<nav>
-    @if (!$isSpecimens)
-        <x-specimens-nav-link href="{{ route('specimens.index') }}" :active="request()->routeIs('specimens.index')">
-            Back to Specimens
-        </x-specimens-nav-link>
-    @endif
+<nav class="bg-gray-100 p-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <!-- Standard navigation links -->
+        <div class="hidden md:flex space-x-4">
+            @if (!$isSpecimens)
+                <x-specimens-nav-link href="{{ route('specimens.index') }}"
+                                      :active="request()->routeIs('specimens.index')">
+                    Back to Specimens
+                </x-specimens-nav-link>
+            @endif
 
-    <x-specimens-nav-link href="{{ route('specimens.create') }}" :active="request()->routeIs('specimens.create')">
-        Add Specimen
-    </x-specimens-nav-link>
+            <x-specimens-nav-link href="{{ route('specimens.create') }}"
+                                  :active="request()->routeIs('specimens.create')">
+                Add Specimen
+            </x-specimens-nav-link>
 
-    <x-specimens-nav-link href="{{ route('character_specimens.index') }}"
-                          :active="request()->routeIs('character_specimens.index')">
-        Characters
-    </x-specimens-nav-link>
+            <x-specimens-nav-link href="{{ route('character_specimens.index') }}"
+                                  :active="request()->routeIs('character_specimens.index')">
+                Characters
+            </x-specimens-nav-link>
 
-    <x-specimens-nav-link href="{{ route('data_source.index') }}" :active="request()->routeIs('data_source.index')">
-        Add Data Source
-    </x-specimens-nav-link>
+            <x-specimens-nav-link href="{{ route('data_source.index') }}"
+                                  :active="request()->routeIs('data_source.index')">
+                Add Data Source
+            </x-specimens-nav-link>
 
-    <x-specimens-nav-link href="{{ route('specimen_cluster.index') }}"
-                          :active="request()->routeIs('specimens_cluster.index')">
-        Manage Clusters
-    </x-specimens-nav-link>
+            <x-specimens-nav-link href="{{ route('specimen_cluster.index') }}"
+                                  :active="request()->routeIs('specimens_cluster.index')">
+                Manage Clusters
+            </x-specimens-nav-link>
 
-    <x-specimens-nav-link href="{{ route('specimen_group.index') }}"
-                          :active="request()->routeIs('specimens_group.index')">
-        Manage Groups
-    </x-specimens-nav-link>
+            <x-specimens-nav-link href="{{ route('specimen_group.index') }}"
+                                  :active="request()->routeIs('specimens_group.index')">
+                Manage Groups
+            </x-specimens-nav-link>
+        </div>
+        <!-- End Standard navigation links -->
+        <!-- Hamburger menu for mobile -->
+        <div class="md:hidden">
+            <button type="button" class="text-gray-300 hover:text-white focus:outline-none"
+                    onclick="toggleMenu('specimens-menu')">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+            </button>
+        </div>
 
-    <!-- Add more links as needed -->
+        <!-- End Mobile Navigation links -->
+        <div id="specimens-menu" class="hidden md:hidden">
+            @if (!$isSpecimens)
+                <x-specimens-nav-link href="{{ route('specimens.index') }}"
+                                      :active="request()->routeIs('specimens.index')">
+                    Back to Specimens
+                </x-specimens-nav-link>
+            @endif
+
+            <x-specimens-nav-link href="{{ route('specimens.create') }}"
+                                  :active="request()->routeIs('specimens.create')">
+                Add Specimen
+            </x-specimens-nav-link>
+
+            <x-specimens-nav-link href="{{ route('character_specimens.index') }}"
+                                  :active="request()->routeIs('character_specimens.index')">
+                Characters
+            </x-specimens-nav-link>
+
+            <x-specimens-nav-link href="{{ route('data_source.index') }}"
+                                  :active="request()->routeIs('data_source.index')">
+                Add Data Source
+            </x-specimens-nav-link>
+
+            <x-specimens-nav-link href="{{ route('specimen_cluster.index') }}"
+                                  :active="request()->routeIs('specimens_cluster.index')">
+                Manage Clusters
+            </x-specimens-nav-link>
+
+            <x-specimens-nav-link href="{{ route('specimen_group.index') }}"
+                                  :active="request()->routeIs('specimens_group.index')">
+                Manage Groups
+            </x-specimens-nav-link>
+        </div>
+        <!-- End Mobile navigation links -->
+    </div>
 </nav>
+
+<script>
+    function toggleMenu(menuId) {
+        var element = document.getElementById(menuId);
+        element.classList.toggle("hidden");
+    }
+</script>
