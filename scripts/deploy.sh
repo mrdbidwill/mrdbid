ba7315a79133d977a943467934374283c4ac7dfd
Will Johnston <129679679+mrdbidwill@users.noreply.github.com>
2024-11-13 06:01:16 -0600

    updated deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,14 +3,10 @@
 # Navigate to project directory
 cd /var/www/public_html/mrdbid || exit
 
-# Install the required PHP extensions
-sudo apt-get install -y php8.3-xml
-
 # Pull latest changes from GitHub repository
 git pull origin main
 
 # Install/Update Composer dependencies as will_user
-sudo -u will_user composer update --optimize-autoloader --no-dev
 sudo -u will_user composer install --optimize-autoloader --no-dev
 
 # Run database migrations
