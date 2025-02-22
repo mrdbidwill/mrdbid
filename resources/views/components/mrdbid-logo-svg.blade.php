23874a68743ee5328436840d6f020319e1227807
Will Johnston <129679679+mrdbidwill@users.noreply.github.com>
2025-02-22 07:27:03 -0600

    change logo into SVG and centered on navigation link line

---
+++ resources/views/components/mrdbid-logo-svg.blade.php
@@ -0,0 +1,15 @@
+<svg width="400" height="60" xmlns="http://www.w3.org/2000/svg" class="inline-flex items-center">
+    <text x="0" y="30" font-size="36" font-weight="bold" letter-spacing="-4">
+        <tspan fill="#DC2626">M</tspan>
+        <tspan fill="#EA580C">R</tspan>
+        <tspan fill="#CA8A04">D</tspan>
+        <tspan fill="#064E3B">B</tspan>
+        <tspan fill="#2563EB">I</tspan>
+        <tspan fill="#4338CA">D</tspan>
+    </text>
+    <text x="0" y="45" font-size="14" font-weight="normal">
+        <tspan fill="#DC2626">M</tspan><tspan style="color: black;">ush</tspan><tspan fill="#EA580C">R</tspan><tspan style="color: black;">oom </tspan>
+        <tspan fill="#CA8A04">D</tspan><tspan>ata</tspan><tspan fill="#064E3B">B</tspan><tspan style="color: black;">ase </tspan>
+        <tspan fill="#2563EB">I</tspan><tspan fill="#4338CA">D</tspan><tspan style="color: black;">entification</tspan>
+    </text>
+</svg>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -7,8 +7,10 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
     @guest
         <!-- Links for guests -->
-        <x-authentication-card-logo  class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
-        ></x-authentication-card-logo>
+        <x-mrdbid-logo-svg  class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+        ></x-mrdbid-logo-svg>
+
+
 
         <x-nav-link href="{{ route('about') }}" :active="request()->routeIs('about')">
             {{ __('About') }}
@@ -32,7 +34,7 @@
 
     @else
         <!-- Link for logged-in users -->
-        <x-authentication-card-logo  class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"></x-authentication-card-logo>
+        <x-mrdbid-logo-svg  class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"></x-mrdbid-logo-svg>
 
         <x-nav-link href="{{ route('about') }}" :active="request()->routeIs('about')">
             {{ __('About') }}
