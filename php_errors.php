79d41e92b91ccd56b1e1890ea940d4a7a3c8001a
Will Johnston <129679679+mrdbidwill@users.noreply.github.com>
2025-03-02 12:22:30 -0600

    set up nginx - remove apache server

--- php_errors.php
+++ php_errors.php
@@ -2,3 +2,17 @@
 [28-Feb-2025 19:18:17 America/Chicago] PHP Warning:  Failed loading Zend extension 'usr/lib/php/20230831/xdebug.so' (tried: /usr/lib/php/20230831/usr/lib/php/20230831/xdebug.so (/usr/lib/php/20230831/usr/lib/php/20230831/xdebug.so: cannot open shared object file: No such file or directory), /usr/lib/php/20230831/usr/lib/php/20230831/xdebug.so.so (/usr/lib/php/20230831/usr/lib/php/20230831/xdebug.so.so: cannot open shared object file: No such file or directory)) in Unknown on line 0
 [01-Mar-2025 01:39:03 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
 [01-Mar-2025 02:39:00 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[01-Mar-2025 11:03:31 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[01-Mar-2025 11:09:01 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[01-Mar-2025 12:34:33 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[01-Mar-2025 16:36:48 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[01-Mar-2025 16:39:08 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[01-Mar-2025 23:14:00 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[01-Mar-2025 23:39:01 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[02-Mar-2025 00:09:01 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[02-Mar-2025 11:07:05 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[02-Mar-2025 11:09:04 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[02-Mar-2025 11:39:04 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[02-Mar-2025 13:16:04 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[02-Mar-2025 16:46:52 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
+[02-Mar-2025 17:09:02 UTC] Xdebug: [Step Debug] Could not connect to debugging client. Tried: localhost:9003 (through xdebug.client_host/xdebug.client_port).
