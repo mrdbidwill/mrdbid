b4d7e9f66f9e31b17a042fc17ac9c9002e38895e
Will Johnston <mrdbidwill@gmail.com>
2025-08-26 19:08:26 -0500

    lookup_types

--- app/controllers/admin/lookup_types_controller.rb
+++ app/controllers/admin/lookup_types_controller.rb
@@ -1,24 +1,63 @@
-class LookupTypesController < ApplicationController
+class Admin::LookupTypesController < Admin::ApplicationController
   before_action :set_lookup_type, only: %i[ show edit update destroy ]
+
+  # GET /lookup_types
   def index
-    @lookup_options = LookupItem.available_for_mushroom_creation
+    authorize LookupType
+    @lookup_types = policy_scope(LookupType.order(:name))
   end
 
+  # GET /lookup_types/1
   def show
+    authorize @lookup_type
   end
 
+  # GET /lookup_types/new
   def new
+    @lookup_type = LookupType.new
+    authorize @lookup_type
   end
 
+  # POST /lookup_types
   def create
+    @lookup_type = LookupType.new(lookup_type_params)
+    authorize @lookup_type
+    if @lookup_type.save
+      redirect_to admin_lookup_type_path(@lookup_type), notice: "Lookup type was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+  # GET /lookup_types/1/edit
   def edit
+    authorize @lookup_type
   end
 
+  # PATCH/PUT /lookup_types/1
   def update
+    authorize @lookup_type
+    if @lookup_type.update(lookup_type_params)
+      redirect_to admin_lookup_type_path(@lookup_type), notice: "Lookup Type was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
+  # DELETE /lookup_types/1
   def destroy
+    authorize @lookup_type
+    @lookup_type.destroy!
+    redirect_to admin_lookup_types_path, notice: "Lookup type was successfully destroyed."
+  end
+
+  private
+
+  def set_lookup_type
+    @lookup_type = LookupType.find(params.expect(:id))
+  end
+
+  def lookup_type_params
+    params.expect(lookup_type: [:name, :description, :comments, :source])
   end
 end
