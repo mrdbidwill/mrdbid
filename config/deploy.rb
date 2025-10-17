c5178fdf3ed4638a5c8258f4347a2a5be4e166ed
Will Johnston <mrdbidwill@gmail.com>
2025-10-17 10:04:10 -0500

    added auto restart code to config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,3 +51,5 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+after "deploy:published", "puma:restart"
