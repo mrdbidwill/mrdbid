9e3dd775d0ce518882fdbf428ec4d8142c438d90
Will Johnston <mrdbidwill@gmail.com>
2025-10-25 07:10:27 -0500

    moved mblist file outside of project

--- db/scripts/mblist/backfill_genera_table_from_mblist.sql
+++
@@ -1,6 +0,0 @@
-INSERT IGNORE INTO genera (mblist_id, taxon_name, created_at, updated_at)
-SELECT DISTINCT mblist_id, taxon_name, NOW(), NOW()
-FROM mb_lists
-WHERE rank_name IN ('gen.', 'gen', 'genus')
-  AND taxon_name IS NOT NULL
-  AND taxon_name != '';
