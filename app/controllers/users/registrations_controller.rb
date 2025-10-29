52eec70ed62d4811a0f383c61ad5e5e45e6a9342
Will Johnston <mrdbidwill@gmail.com>
2025-10-29 18:15:43 -0500

    Fix account deletion by disabling strict_loading before destroy
    The User model had strict_loading enabled globally, which prevented
    lazy loading of associations during deletion. Override the destroy
    method in RegistrationsController to disable strict_loading for the
    user being deleted, allowing Rails to properly load and delete
    associated records.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
 
+  # Override destroy to handle strict_loading
+  def destroy
+    resource.strict_loading!(false)
+    super
+  end
+
   protected
 
   def configure_permitted_parameters
