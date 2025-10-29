22db942f34168178f0ea4eed61bb616498732ec5
Will Johnston <mrdbidwill@gmail.com>
2025-10-29 15:10:03 -0500

    Fix user account deletion by adding dependent destroy to user_roles association
    This prevents foreign key constraint errors when deleting user accounts.

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,7 @@ class User < ApplicationRecord
   has_many :all_groups, dependent: :destroy
   has_many :clusters, dependent: :destroy
   has_many :projects, dependent: :destroy
-  has_many :user_roles
+  has_many :user_roles, dependent: :destroy
   has_many :roles, through: :user_roles
 
 end
