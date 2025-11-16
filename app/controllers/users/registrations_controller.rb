c6029175b85f4882485806bc1dca162209d9676f
Will Johnston <mrdbidwill@gmail.com>
2025-11-15 18:53:50 -0600

    Reconnect honeypot and Remember Me

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
+  before_action :check_honeypot, only: [:create]
 
   # Override destroy to handle strict_loading
   def destroy
@@ -14,5 +15,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:display_name])
+  end
+
+  private
+
+  def check_honeypot
+    # If the honeypot field is filled, it's likely a bot
+    if params[:user][:website_url].present?
+      Rails.logger.warn "Bot registration attempt detected: #{params[:user][:email]}"
+      redirect_to new_user_registration_path, alert: "Registration failed. Please try again."
+    end
   end
 end
