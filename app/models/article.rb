f2b765cbfd17a3a8049e9ab567f5388c8c9d2559
Will Johnston <mrdbidwill@gmail.com>
2025-12-12 08:10:26 -0600

    added enhancements to Articles to allow for improved Obfuscation process here instead of elsewhere

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,21 @@
 class Article < ApplicationRecord
+  # Predefined subjects to prevent taxonomy sprawl
+  SUBJECTS = [
+    "Mycology",
+    "Mycology - Identification",
+    "Mycology - Obfuscation",
+    "Mushroom Character Discussion",
+    "Tree - Identification",
+    "Plant - Identification",
+    "Website Organization",
+    "Site Documentation",
+    "Technical - Website Issues",
+    "Other"
+  ].freeze
+
   validates :title, :slug, :body, presence: true
   validates :slug, uniqueness: true
+  validates :subject, inclusion: { in: SUBJECTS, allow_blank: true }
   # Keep slugs URL-friendly
   before_validation :normalize_slug
 
